0610cfbc4defbde4e6b9cc14897d49efbe5cb658
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-04-17 14:30:36 +0200

    [Example] Add initial UI

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
