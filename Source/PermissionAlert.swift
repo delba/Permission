63aa1d7b95019d5ad4aa8d53e0c4365526b70d8f
Justin Makaila <justinmakaila@gmail.com>
2016-07-11 14:53:29 -0400

    Changed the bold option on the Permission alert

--- Source/PermissionAlert.swift
+++ Source/PermissionAlert.swift
@@ -64,7 +64,7 @@ public class PermissionAlert {
     var controller: UIAlertController {
         let controller = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: cancelActionTitle, style: .Cancel, handler: cancelHandler)
+        let action = UIAlertAction(title: cancelActionTitle, style: .Default, handler: cancelHandler)
         controller.addAction(action)
         
         return controller
@@ -132,7 +132,7 @@ internal class PrePermissionAlert: PermissionAlert {
     override var controller: UIAlertController {
         let controller = super.controller
         
-        let action = UIAlertAction(title: defaultActionTitle, style: .Default, handler: confirmHandler)
+        let action = UIAlertAction(title: defaultActionTitle, style: .Cancel, handler: confirmHandler)
         controller.addAction(action)
         
         return controller
