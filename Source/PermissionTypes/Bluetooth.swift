9ee46b99b8706bfa696b3b04495f64fca903eca9
Jake Rockland <jakerockland@gmail.com>
2016-11-02 19:12:01 -0700

    .Bluetooth patch (master)
    This addresses #36 and replaces #74 which was closed due to swift-3.0 being merged to master.

--- Source/PermissionTypes/Bluetooth.swift
+++ Source/PermissionTypes/Bluetooth.swift
@@ -32,20 +32,27 @@ internal let BluetoothManager = CBPeripheralManager(
 )
 
 extension Permission {
-    var statusBluetooth: PermissionStatus {
-        let state = (BluetoothManager.state, CBPeripheralManager.authorizationStatus())
+        let status = CBPeripheralManager.authorizationStatus()
         
-        switch state {
-        case (.unsupported, _), (.poweredOff, _), (_, .restricted):
+        switch status {
+        case .notDetermined:
+            return .notDetermined;
+        case .restricted:
             return .disabled
-        case (.unauthorized, _), (_, .denied):
+        case .denied:
             return .denied
-        case (.poweredOn, .authorized):
-            return .authorized
-        default:
-            return .notDetermined
+        case .authorized:
+            let state = BluetoothManager.state
+            
+            switch state {
+            case .unknown, .resetting, .unsupported, .poweredOff:
+                return .disabled
+            case .unauthorized:
+                return .denied
+            case .poweredOn:
+                return .authorized
+            }
         }
-    }
     
     func requestBluetooth(_ callback: Callback?) {
         UserDefaults.standard.requestedBluetooth = true
@@ -62,8 +69,10 @@ extension Permission: CBPeripheralManagerDelegate {
 
 extension CBPeripheralManager {
     func request(_ permission: Permission) {
-        startAdvertising(nil)
-        stopAdvertising()
+        if self.state == .poweredOn {
+            startAdvertising(nil)
+            stopAdvertising()
+        }
     }
 }
 #endif
