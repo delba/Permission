eb4b7fdb14e9dae7cf82f3fc0b01b16378af0803
delba <delba@users.noreply.github.com>
2016-11-18 18:05:44 +0100

    Remove CocoaPods warning about default case never used

--- Source/PermissionTypes/Location.swift
+++ Source/PermissionTypes/Location.swift
@@ -52,10 +52,14 @@ extension CLLocationManager {
         
         requestedLocation = true
         
-        switch permission.type {
-        case .locationAlways: requestAlwaysAuthorization()
-        case .locationWhenInUse: requestWhenInUseAuthorization()
-        default: break
+        if case .locationAlways = permission.type {
+            requestAlwaysAuthorization()
+            return
+        }
+        
+        if case .locationWhenInUse = permission.type {
+            requestWhenInUseAuthorization()
+            return
         }
     }
 }
