c1a895290f4969e12a8792fdb80e531cf6ccf37b
damien <damien@delba.io>
2019-08-17 22:16:26 +0200

    Update year

--- Source/Set.swift
+++ Source/Set.swift
@@ -1,7 +1,7 @@
 //
 // Set.swift
 //
-// Copyright (c) 2015-2016 Damien (http://delba.io)
+// Copyright (c) 2015-2019 Damien (http://delba.io)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
